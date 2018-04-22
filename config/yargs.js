35410caf429c95a95e9997422a9a9926c04913af
Angel Hernandez <ahm@Angels-MBP.home>
2018-04-22 18:43:33 -0400

    Todo app using node

---
+++ config/yargs.js
@@ -0,0 +1,34 @@
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        }
+    })
+    .command('actualizar', 'Actualiza un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente la tarea'
+        }
+    })
+    .command('borrar', 'Borrar una tarea del listado', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Borrar un elemento del listado'
+        }
+    })
+    .help()
+    .argv;
+
+
+module.exports = {
+    argv
+}
